bed1e26aae3201e2dfdd89d66857538ba60ba128
Marco Antonio Santana <marco@santana.nyc>
2024-08-12 15:28:52 -0400

    ModManager get so long personal script

--- init/zsh/install.sh
+++ init/zsh/install.sh
@@ -3,13 +3,15 @@
 sudo pacman -S --noconfirm zsh zsh-{autosuggestions,completions,doc,history-substring-search,lovers,syntax-highlighting} zshdb
 
 # this might change
+mkdir -p ~/.zsh
 ln -s $(pwd)/aliases.zsh ~/.zsh/
 ln -s $(pwd)/archlinux_extras.zsh ~/.zsh/
+mv ~/.zshrc ~/.zshrc.bak($date)
 ln -s $(pwd)/zshrc ~/.zshrc
 
 # change to equals I've been growing anxious about repeatedly adding rights 
-chmod ug+x journal.sh
-chmod ug+x cloudsave.sh
+# chmod ug+x journal.sh
+# chmod ug+x cloudsave.sh
 
 # help me prevent running this more than once
-chmod ug-x install.sh
+# chmod ug-x install.sh
