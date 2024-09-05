102cd2121ea6e3775b44122ffd09614966e87a7b
Marco Antonio Santana <marco@santana.nyc>
2024-09-05 09:14:58 -0400

    removed rendundant code

--- init/bash/install.sh
+++ init/bash/install.sh
@@ -8,8 +8,5 @@ echo "# my bash configs" >>  ~/.bashrc
 echo "#" >>  ~/.bashrc
 echo "source $(pwd)/bash.conf" >> ~/.bashrc
 
-chmod ug+x journal.sh
-chmod ug+x cloudsave.sh
-
 # help me prevent running this more than once
 chmod ug-x install.sh
