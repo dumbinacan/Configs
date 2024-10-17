5df657e15c6dc2b62ded986568035139e14b9f34
Marco Antonio Santana <marco@santana.nyc>
2024-10-16 22:27:34 -0400

    ssh and more clean up

--- init/zsh/aliases.zsh
+++ init/zsh/aliases.zsh
@@ -1,10 +1,12 @@
 # aliases.zsh
+
 alias sudo='sudo '
 alias dd='dd status=progress'
 alias ping='ping -c3'
-
 alias journal='~/Configs/init/zsh/journal.sh'
-
-alias storagebin='ssh -i /home/marco/.ssh/keys/ourbeefyserver/storageb-in marco@storageb.in'
-
 alias cmatrix='neo-matrix'
+
+# Temporary to know why it might fail while setting up my network
+alias ssh="echo '/etc/clamav/clamd.conf has AlertPhishingSSLMismatch enabled and may cause false positives' && ssh"
+# SSH shortcuts
+alias storagebin='ssh marco@storageb.in'
