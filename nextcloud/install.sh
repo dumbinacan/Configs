519d6c98d5d3d557e5e635ef4c99f7e37670e83b
Marco Antonio Santana <marco@santana.nyc>
2024-10-16 23:32:40 -0400

    stop storing password in plaintext

--- nextcloud/install.sh
+++ nextcloud/install.sh
@@ -2,21 +2,22 @@
 
 # this script depends on nextcloudcmd
 # for arch, install nextcloud-client
+# This script will now use gpg to store the password encrypted
 
 # get credentials
 echo "Please enter your"
+read -p  'gpg email:' email
 read -p  'Nextcloud Server: ' server_uri
 read -p  'Username: ' user
 read -sp 'Password: ' password
 client_root=$HOME
-
+password=$(echo $password | gpg -e -a -r $email)
 # make ~/.credentials for future use
 echo '#!/bin/sh' > $client_root/.credentials
 echo "user=$user" >> $client_root/.credentials
-echo "password=$password" >> $client_root/.credentials
+echo "password=""$""(""echo '$password'" "|" "gpg -dq)" >> $client_root/.credentials
 echo "client_root=$client_root" >> $client_root/.credentials
 echo "server_uri=$server_uri" >> $client_root/.credentials
 
 # minimize who can see this file
-chmod a-rwx,ug+r $client_root/.credentials
-chmod ug+x Documents Pictures Notes Configs
+chmod 600 $client_root/.credentials
