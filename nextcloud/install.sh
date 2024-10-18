89d416782fc51e4cfe605f236f00a76379ce9fa3
Marco Antonio Santana <marco@santana.nyc>
2024-10-18 11:17:41 -0400

    I don't wanna type my password all the time

--- nextcloud/install.sh
+++ nextcloud/install.sh
@@ -9,16 +9,17 @@ client_root=$HOME
 
 # get credentials
 echo "Please enter your"
-read -p  'Nextcloud Server: ' server_uri
+read -p  'Nextcloud Server: https://' server_uri && server_uri="https://$server_uri"
 read -p  'Username: ' user
-read -sp 'Password: ' password
-echo
-password=$(echo $password | openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt)
+read -sp 'Password: ' password && echo
+
+# Encrypt password
+password=$(echo $password | openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -pass pass:'adding-one-extra-step-to-viewing-this-passowrd')
 
 # make ~/.credentials for future use
 echo '#!/bin/sh' > $client_root/.credentials
 echo "user=$user" >> $client_root/.credentials
-echo "password=""$""(""echo '$password'" "|" "openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt)" >> $client_root/.credentials
+echo "password=""$""(""echo '$password'" "|" "openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:'adding-one-extra-step-to-viewing-this-passowrd')" >> $client_root/.credentials
 echo "client_root=$client_root" >> $client_root/.credentials
 echo "server_uri=$server_uri" >> $client_root/.credentials
 
