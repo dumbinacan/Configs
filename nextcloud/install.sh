766e18e58aa3dafa7b6a381072396bae6dba8f29
Marco Antonio Santana <marco@santana.nyc>
2024-10-18 03:57:48 -0400

    swap gpg with sshpass

--- nextcloud/install.sh
+++ nextcloud/install.sh
@@ -2,20 +2,23 @@
 
 # this script depends on nextcloudcmd
 # for arch, install nextcloud-client
-# This script will now use gpg to store the password encrypted
+# requires openssl and sshpass
+pacman -S openssl sshpass
+
+client_root=$HOME
 
 # get credentials
 echo "Please enter your"
-read -p  'gpg email:' email
 read -p  'Nextcloud Server: ' server_uri
 read -p  'Username: ' user
 read -sp 'Password: ' password
-client_root=$HOME
-password=$(echo $password | gpg -e -a -r $email)
+echo
+password=$(echo $password | openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt)
+
 # make ~/.credentials for future use
 echo '#!/bin/sh' > $client_root/.credentials
 echo "user=$user" >> $client_root/.credentials
-echo "password=""$""(""echo '$password'" "|" "gpg -dq)" >> $client_root/.credentials
+echo "password=""$""(""echo '$password'" "|" "openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt)" >> $client_root/.credentials
 echo "client_root=$client_root" >> $client_root/.credentials
 echo "server_uri=$server_uri" >> $client_root/.credentials
 
