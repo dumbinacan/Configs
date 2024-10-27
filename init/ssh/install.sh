0fbec08687fe64109eb4a1dc8cf590cbe3d0dd37
Marco Antonio Santana <marco@santana.nyc>
2024-10-26 22:46:15 -0400

    added key directory structure

--- init/ssh/install.sh
+++ init/ssh/install.sh
@@ -3,3 +3,4 @@
 mkdir -p ~/.ssh
 sudo mv ~/.ssh/config ~/.ssh/config.$(date +%Y-%m-%d).bak # backup in case there was something else there.
 sudo ln -s $(pwd)/config ~/.ssh/config
+cp -r keys ~/.ssh/
