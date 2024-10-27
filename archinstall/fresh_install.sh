df464a81761c545e15d5a0633c278cb797cfdfa4
Marco Antonio Santana <marco@santana.nyc>
2024-10-26 22:51:26 -0400

    FUCK git

--- archinstall/fresh_install.sh
+++ archinstall/fresh_install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+
 # I think I want to call a bunch of scripts to set up everything
 # moving to flatpak, so installing the base shit I want on flat pak should go here
 # https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz
@@ -7,6 +8,30 @@
 # moving back to the nextcloud gui
 # maybe some sort of reminder of the directories for nextcloud idk
 # this script should set up all the configs
-# install yay
+# install yay?
 # install all the flatpaks
 mkdir -p ~/Notes ~/Projects
+#
+# TODO
+# the archimedes json needs to be fixed for the drive its using.
+# as is it only has 256 Gigs
+#
+# I want a core set of utils and shit installed
+# I decided that the KDE apps will hard installed in the system for example
+# the first gigantic blob down there is from installing spectacle and all its optional dependencies
+#                                             (and it's optional dependencies optional dependencies)
+# add to package list
+# clamav
+# partitionmanager
+# spectacle
+# vtk glew hdf5
+# adios2 cgns fmt gdal gl2ps gnuplot graphviz libharu liblas mariadb-libs netcdf opencascade openimagedenoise openmpi openvdb openvr ospray pdal postgresql-libs proj python-matplotlib python-mpi4py qt6-declarative qt5-declarative tk unixodbc
+# openpmix-docs rdma-core cuda rocm-language-runtime prrte-docs hip-runtime-amd gcc-fortran openucc python-setuptools arrow cfitsio podofo libid3tag libspectre gssdp gssdp-docs libnice gtk4 gtk4-docs webkit2gtk mono r xterm netcdf-cxx netcdf-fortran glfw intel-compute-runtime level-zero-headers level-zero-loader openmp benchmark blas-openblas blas64-openblas python-brotli python-brotlicffi python-fs python-lxml-html-clean python-lxml-docs python-lxml python-lz4 python-pyqt5-webengine python-pyqt5-sip python-pyqt5 python-pyqt6 python-pyqt6-3d python-pyqt6-charts python-pyqt6-datavisualization python-pyqt6-networkauth python-pyqt6-sip python-pyqt6-webengine python-reportlab python-scipy python-sympy python-unicodedata2 python-zopfli python-railroad-diagrams python-jinja python-gobject python-wxpython python-cairo python-cairocffi python-tornado imagemagick
+# libraw libultrahdr libwmf djvulibre jasper jasper-doc
+# gwenview
+# qt6-imageformats kimageformats kamera
+# jxrlib
+# okular
+# kdegraphics-mobipocket
+# unarchiver
+# unrar-free
--- ranger/install.sh
+++ ranger/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # requirements
-# pacman -S ueberzug
+sudo pacman -S --noconfirm ueberzug
 mv ~/.config/ranger/rc.conf ~/.config/ranger/rc.conf.$(date +%Y-%m-%d).bak
 ln -s $(pwd)/rc.conf ~/.config/ranger/rc.conf
