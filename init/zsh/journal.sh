41ff508a5d7b174887691cdd235e8154a142dc7a
Marco Antonio Santana <marco@santana.nyc>
2025-02-06 11:10:26 -0500

    I DON'T FUCKING KNOW. DON'T ASK!

--- init/zsh/journal.sh
+++ init/zsh/journal.sh
@@ -1,23 +1,14 @@
 #!/bin/bash
-# puts you in a vim text file which
-# corresponds to today's date in the journal folder
-# auto update to the cloud in the background
+# journal your thoughts
+# save by name or date
+# organized in a folder structure that keeps track of the day you wrote it
 
-# update Notes folder before running anything
-# to ensure that we are writing in the most up to date file
-## $HOME/Configs/init/bash/cloudsave.sh
-# the command above is really slow
-# I want to make this update the specific file we are accessing only
-
-# path to the correct year folder 
-# TODO
 path="$HOME/Notes/journal/$(date +"%Y")/$(date +"%m")/$(date +"%d")"
 if [[ $1 ]]; then
   filename=$1
 else
   filename="$(date +"%Y-%m-%d")"
 fi
+
 mkdir -p $path
 vim $path/$filename
-# TODO
-# optionally take user input for a filename
