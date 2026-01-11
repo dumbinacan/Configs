54f2ae822861b144f6e94d872e09b0927cdb7f32
Marco Antonio Santana <marco@santana.nyc>
2026-01-10 20:55:10 -0500

    lord forgive me

--- init/ssh/install.sh
+++ init/ssh/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/.ssh
-sudo mv ~/.ssh/config ~/.ssh/config.$(date +%Y-%m-%d).bak # backup in case there was something else there.
-sudo ln -s $(pwd)/config ~/.ssh/config
-cp -r keys ~/.ssh/
+                                                   # forgive me
+mv ~/.ssh/config ~/.ssh/config.$(date +%Y-%m-%d).bak 2> /dev/null # backup in case there was something else there.
+ln -s $(pwd)/config ~/.ssh/config
+# cp -r keys ~/.ssh/
