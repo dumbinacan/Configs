ca23fff5c7e3ca3607e68b1d8ae40c0f2c41b2ca
Marco Antonio Santana <marco@santana.nyc>
2026-01-30 01:17:23 -0500

    ask me to remember

--- init/zsh/aliases.zsh
+++ init/zsh/aliases.zsh
@@ -5,9 +5,3 @@ alias dd='dd status=progress'
 alias ping='ping -c3'
 alias journal='~/Configs/init/zsh/journal.sh'
 alias cmatrix='neo-matrix'
-
-# Temporary to know why it might fail while setting up my network
-alias ssh="echo '/etc/clamav/clamd.conf has AlertPhishingSSLMismatch enabled and may cause false positives' && ssh"
-# SSH shortcuts
-alias storagebin='ssh marco@storageb.in'
-alias forgejo='ssh marco@git.santana.nyc'
